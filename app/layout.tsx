084abe7ad04a0acaf34705fb626683f211cee5f0
Jose Conde <145078308+LemurTech22@users.noreply.github.com>
2025-10-12 13:15:43 -0500

    added analytics for vercel and more descriptions about projects

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import {Analytics} from "@vercel/analytics/next"
 
 const outfit = Outfit({
   subsets: ["latin"], weight: ["400","500","600","700"]
@@ -21,7 +23,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden`}>
         <Navbar />
-        <main>{children}</main>
+        <main>
+          {children}
+          <Analytics/>
+          <SpeedInsights/>
+          </main>
       </body>
     </html>
   );
