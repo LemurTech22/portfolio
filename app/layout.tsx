b8408886cdfab2360fe1daf176d12dcff5817a26
Jose Conde <145078308+LemurTech22@users.noreply.github.com>
2025-10-12 14:02:33 -0500

    packages stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar"
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import {Analytics} from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next"
 
 const outfit = Outfit({
   subsets: ["latin"], weight: ["400","500","600","700"]
